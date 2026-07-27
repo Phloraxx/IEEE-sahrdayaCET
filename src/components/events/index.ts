b7e36deb0b7343032bd1ca56e0c2183900e9dc39
Sourav P Bijoy <71513365+Phloraxx@users.noreply.github.com>
2026-07-27 18:11:10 +0530

    feat(events): export Infinia teaser section

--- src/components/events/index.ts
+++ src/components/events/index.ts
@@ -1,3 +1,4 @@
 export { EventHeroSection } from './EventHeroSection';
 export { EventDetailModal } from './EventDetailModal';
 export { EventListSection } from './EventListSection';
+export { InfiniaTeaserSection } from './InfiniaTeaserSection';
